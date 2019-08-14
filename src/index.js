d70c735014c6ebf795278b2eda75c86e78e79288
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-08-14 15:19:34 -0500

    created store with mock reducer func in place

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import 'bulma/css/bulma.css';
+import { Provider } from 'react-redux'; 
+import { createStore } from 'react'; 
+// import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(reducer); 
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider state={state}>
+    <App />
+</Provider>, rootElement);
