3ee559af1686d7757cafb5e68c188565594c7d7a
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-08-14 15:22:17 -0500

    redux store setup in index.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { reducerFunc } from './reducers' 
 import { Provider } from 'react-redux'; 
 import { createStore } from 'react'; 
 // import 'bulma/css/bulma.css';
 import './styles.scss';
 
-const store = createStore(reducer); 
+const store = createStore(reducerFunc); 
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
-<Provider state={state}>
+<Provider store={store}>
     <App />
 </Provider>, rootElement);
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+export const reducerFunc = () => {
+    
+    return (  //when do I need the state to be edited? 
+        <div>
+            <p>Reducer function</p>
+        </div>
+    )
+}
